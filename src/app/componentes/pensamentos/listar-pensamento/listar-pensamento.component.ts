05aafb961579d83a2778a42f8ed45e9107d19262
Wesley <wmixgamer@gmail.com>
2023-07-20 10:28:31 -0300

    :label: implementing interfaces

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.scss']
 })
 export class ListarPensamentoComponent {
-  listaPensamentos = []
+  listaPensamentos: Pensamento[] = []
 }
